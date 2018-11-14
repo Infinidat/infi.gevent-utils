8a7453a212cc220e413268fc6723438cd360eefe
Amit Oren <aoren@infinidat.com>
2018-11-14 11:28:44 +0200

    HOSTDEV-2913 use six to reraise the encapsulated exception

--- src/infi/gevent_utils/deferred.py
+++ src/infi/gevent_utils/deferred.py
@@ -1,21 +1,6 @@
 import sys
+import six
 import gevent
-
-
-__PY3 = sys.version_info[0] == 3
-
-
-def _py3_raise_value(result):
-    return result[0](result[1]).with_traceback(result[2])
-
-
-def _py2_raise_value(result):
-    return result[0], result[1], result[2]
-
-if __PY3:
-    _to_raise_value = _py3_raise_value
-else:
-    _to_raise_value = _py2_raise_value
 
 
 def create_threadpool_executed_func(original_func):
@@ -37,7 +22,7 @@ def create_threadpool_executed_func(original_func):
         if status:
             return result
         else:
-            raise(_to_raise_value(result))
+            six.reraise(*result)
     new_func.__name__ = original_func.__name__
     new_func.__doc__ = "(gevent-friendly)" + (" " + original_func.__doc__ if original_func.__doc__ is not None else "")
     return new_func
