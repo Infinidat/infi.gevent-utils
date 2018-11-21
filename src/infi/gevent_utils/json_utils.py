f462d2c56ecd12f97b1c3ed91fd316c8f58347cc
Amit Oren <aoren@infinidat.com>
2018-11-21 11:41:00 +0200

    TRIVIAL fix typo in comment in json_utils

--- src/infi/gevent_utils/json_utils.py
+++ src/infi/gevent_utils/json_utils.py
@@ -40,7 +40,7 @@ def can_dumps_sort_keys():
 
 def encode(python_object, indent=None, large_object=False):
     """:returns: a JSON-representation of the object"""
-    # sorted keys is eaiser to read; however, Python-2.7.2 does not have this feature
+    # sorted keys is easier to read; however, Python-2.7.2 does not have this feature
     kwargs = dict(indent=indent)
     if can_dumps_sort_keys():
         kwargs.update(sort_keys=True)
